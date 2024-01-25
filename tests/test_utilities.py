0e0705e2a7ca4989139934cf61539b572efcbf45
Patrick Nasr <nasr7601@hotmail.com>
2024-01-25 19:32:24 +1100

    fix failing test

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -2,4 +2,4 @@ from flask_server.src.utilities import process_integer
 
 # pytest tests/
 def test_processing_integer():
-    assert process_integer(3) == 6
+    assert process_integer(3, 'double') == 6
