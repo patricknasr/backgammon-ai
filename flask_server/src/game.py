4456b5af360f039fe49ad8d635a3b563d46ffb84
Patrick Nasr <nasr7601@hotmail.com>
2024-01-26 13:19:21 +1100

    add functionality

--- flask_server/src/game.py
+++ flask_server/src/game.py
@@ -17,18 +17,6 @@ class Point:
 @dataclass
 class Board:
     points: list[Point] = field(default_factory=list)
-
-def roll_dice() -> tuple[int, int]:
-        return (random.randint(1, 6), random.randint(1, 6))
-
-def computer_piece_type(player_piece_type: Piece) -> Piece:
-        if player_piece_type == Piece.LIGHT:
-            return Piece.DARK
-        elif player_piece_type == Piece.DARK:
-            return Piece.LIGHT
-        else:
-            # raise InvalidColour
-            pass
     
 class BoardView:
     def __init__(self):
@@ -51,34 +39,47 @@ class BoardView:
         self._board.points[new_position].pieces.append(move_colour)
 
         return True
-    
-    def has_game_been_won(self) -> bool:
-        if len(self._board.points[MIN_POINT_POS].pieces) == 15 or len(self._board.points[MAX_POINT_POS].pieces) == 15:
-            return True
-        
-        return False
-    
-    def point_is_empty(self, point_index: int) -> bool:
-        if len(self._board.points[point_index].pieces) == 0:
-            return True
-        elif len(self._board.points[point_index].pieces) > 0:
-            return False
-        else:
-            # raise NegativePiecesCountAtPoint
-            pass
 
-    def can_place_on_point(self, player_piece_type: Piece, point_index: int) -> bool:
-        if self.point_is_empty(point_index) or \
-            len(self._board.points[point_index].pieces) == 1 \
-                or self._board.points[point_index].pieces[0] is not computer_piece_type(player_piece_type):
-                # something about being in bounds \
-            return True
+def roll_dice() -> tuple[int, int]:
+        return (random.randint(1, 6), random.randint(1, 6))
+
+def computer_piece_type(player_piece_type: Piece) -> Piece:
+        if player_piece_type == Piece.LIGHT:
+            return Piece.DARK
+        elif player_piece_type == Piece.DARK:
+            return Piece.LIGHT
+        else:
+            # raise InvalidColour
+            pass
+        
+def has_game_been_won(board_view: BoardView) -> bool:
+    if len(board_view._board.points[MIN_POINT_POS].pieces) == 15 or len(board_view._board.points[MAX_POINT_POS].pieces) == 15:
+        return True
     
+    return False
+
+def point_is_empty(board_view: BoardView, point_index: int) -> bool:
+    if len(board_view._board.points[point_index].pieces) == 0:
+        return True
+    elif len(board_view._board.points[point_index].pieces) > 0:
         return False
-    
-    def print_board_to_cli(self):
+    else:
+        # raise NegativePiecesCountAtPoint
+        pass
+
+def can_place_on_point(board_view: BoardView, player_piece_type: Piece, point_index: int) -> bool:
+    if board_view.point_is_empty(point_index) or \
+        len(board_view._board.points[point_index].pieces) == 1 \
+            or board_view._board.points[point_index].pieces[0] is not computer_piece_type(player_piece_type):
+            # something about being in bounds \
+        return True
+
+    return False
+
+
+def print_board_to_cli(board_view: BoardView):
         print("\n")
-        for idx, point in enumerate(self._board.points):
+        for idx, point in enumerate(board_view._board.points):
             light_count = point.pieces.count(Piece.LIGHT)
             dark_count = point.pieces.count(Piece.DARK)
 
@@ -92,9 +93,23 @@ class BoardView:
 
 def main():
     initialise_board = BoardView()
-    initialise_board.print_board_to_cli()
-    initialise_board.move_piece(move_colour=Piece.LIGHT, original_position=1, move_by=1)
-    initialise_board.print_board_to_cli()
+    print_board_to_cli(initialise_board)
+    (die_a, die_b) = roll_dice()
+    initialise_board.move_piece(move_colour=Piece.LIGHT, original_position=1, move_by=die_a)
+    initialise_board.move_piece(move_colour=Piece.LIGHT, original_position=1, move_by=die_b)
+    print_board_to_cli(initialise_board)
+    (die_a, die_b) = roll_dice()
+    initialise_board.move_piece(move_colour=Piece.DARK, original_position=24, move_by=die_a)
+    initialise_board.move_piece(move_colour=Piece.DARK, original_position=24, move_by=die_b)
+    print_board_to_cli(initialise_board)
+    (die_a, die_b) = roll_dice()
+    initialise_board.move_piece(move_colour=Piece.LIGHT, original_position=1, move_by=die_a)
+    initialise_board.move_piece(move_colour=Piece.LIGHT, original_position=1, move_by=die_b)
+    print_board_to_cli(initialise_board)
+    (die_a, die_b) = roll_dice()
+    initialise_board.move_piece(move_colour=Piece.DARK, original_position=24, move_by=die_a)
+    initialise_board.move_piece(move_colour=Piece.DARK, original_position=24, move_by=die_b)
+    print_board_to_cli(initialise_board)
 
 if __name__ == "__main__":
     main()
